88d27c308f4200be386ec4af54fc61ba0465cff1
Raphael Stedler <r.stedler@gmx.de>
2023-01-11 15:06:41 +0100

    imports changed

--- index.py
+++ index.py
@@ -1,8 +1,8 @@
 # index page
 import json
 import requests
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 from dash.dependencies import Input, Output, State
 import dash_bootstrap_components as dbc
 
